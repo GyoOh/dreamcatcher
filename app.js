62367ffc697c770f7ed8e08ca818efa0aa50c15f
Sejin Oh <soh52@my.bcit.ca>
2022-04-25 13:19:14 -0700

    first

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const app = express();
+const authentication = require("./routes/authentication");
+const posts = require("./routes/posts");
+const subs = require("./routes/subs");
+const comments = require("./routes/comments");
+
+const db = require("./fake-db");
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(cookieSession({
+  name: 'whoami',
+  httpOnly: "true",
+  keys: ['key1', 'key2'],
+  maxAge: 24 * 60 * 60 * 1000 //24hrs
+}))
+
+app.use("/authentication", authentication);
+app.use("/posts", posts);
+app.use("/subs", subs);
+app.use("/comments", comments);
+
+app.get("/", (req, res) => {
+  let posts = db.getPosts();
+  let user = db.getUserByUsername(req.session.whoami);
+  for (const post of posts) {
+    let user = db.getUser(post.creator);
+    post.creatorName = user.uname;
+  }
+  res.render("index", { posts, user });
+})
+
+module.exports = app;
---
+++ routes/posts.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../fake-db");
+
+
+router.get("/show/:postid", (req, res) => {
+    let postId = req.params.postid;
+    let userVoteScore = null;
+    let post = db.getPost(postId);
+    let user = db.getUserByUsername(req.session.whoami);
+    let voteScore = db.getVotesForPost(Number(postId));
+    let postThatWeVoted = voteScore
+    .filter(vote => vote.post_id === postId)
+    .find(vote => vote.user_id === user.id);
+    if (!postThatWeVoted) {
+        userVoteScore = 0;
+    } else {
+        userVoteScore = postThatWeVoted.value;
+    }
+    let netTotal = voteScore.reduce((prev, curr) => prev + curr.value, 0)
+    console.log(netTotal);
+    res.render("post", { post, user, netTotal, userVoteScore });
+})
+
+router.get("/create", (req, res) => {
+    let id = req.session.whoami;
+    let user = db.getUserByUsername(id);
+    res.render("create", { user });
+})
+
+router.post("/create", (req, res) => {
+    let id = req.session.whoami;
+    let user = db.getUserByUsername(id);
+    let data = req.body;
+    let title = data.title;
+    let link = data.link;
+    let creator = user.id;
+    let description = data.description;
+    let subgroup = data.subgroup;
+    db.addPost(title, link, creator, description, subgroup);
+    res.redirect("/");
+})
+
+router.get("/edit/:postid", (req, res) => {
+    let postId = req.params.postid;
+    let post = db.getPost(postId);
+    let id = req.session.whoami
+    let user = db.getUserByUsername(id);
+    res.render("edit", { post, user });
+})
+
+router.post("/edit/:postid", (req, res) => {
+    let postId = req.params.postid;
+    let editedData = req.body;
+    let id = req.session.whoami;
+    let user = db.getUserByUsername(id);
+    let post = db.getPost(postId);
+    if (user.id === post.creator.id) {
+        db.editPost(postId, editedData);
+        res.redirect(`/posts/show/${postId}`);
+    } else {
+        res.redirect("/");
+    }
+})
+
+router.get("/deleteconfirm/:postid", (req, res) => {
+    let postId = req.params.postid;
+    let post = db.getPost(postId);
+    let id = req.session.whoami;
+    let user = db.getUserByUsername(id);
+    res.render("delete", { user, post });
+})
+
+router.post("/delete/:postid", (req, res) => {
+    let postId = req.params.postid;
+    let post = db.getPost(postId);
+    let postCreator = post.creator;
+    let id = req.session.whoami;
+    let user = db.getUserByUsername(id);
+    if (postCreator.id === user.id) {
+        db.deletePost(post.id);
+        res.redirect("/subs/list");
+    } else {
+        res.redirect(`/posts/show/${postId}`);
+    }
+})
+
+router.post("/comment-create/:postid", (req, res) => {
+    let postId = req.params.postid;
+    let data = db.getPost(postId);
+    let user = db.getUserByUsername(req.session.whoami);
+    let creator = user.id;
+    let description = req.body.comment;
+    db.addComment(postId, creator, description);
+    res.redirect(`/posts/show/${postId}`)
+})
+
+router.post("/vote/:postid", (req, res) => {
+    let postId = req.params.postid;
+    let vote = req.body.setvoteto;
+    let id = req.session.whoami;
+    let post = db.getPost(postId);
+    let user = db.getUserByUsername(id);
+    db.addVoteForPost(postId, user.id, Number(vote));
+    res.redirect(`/posts/show/${postId}`)
+})
+
+module.exports = router;
