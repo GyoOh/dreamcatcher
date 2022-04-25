3b2a0a5627f3c2928ccc93720f31653e517e732d
Gwanggyo Oh <goh3@my.bcit.ca>
2022-04-25 15:10:06 -0700

    changed

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const app = require('./app.js');
 const express = require("express");
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`server should be running at http://localhost:${PORT}/`))
+app.listen(port, () => console.log(`server should be running at http://localhost:${port}/`))
