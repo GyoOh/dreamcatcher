a0c1afd4d1387ecd7e368d098a63ef9b323cdfc3
Gwanggyo Oh <goh3@my.bcit.ca>
2022-04-26 14:07:00 -0700

    still database

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,28 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../fake-db");
+const dbModel = require("../databaseAccessLayer")
+const database = require("../databaseConnection");
 
-router.post("/login", (req, res) => {
-    let givenUsername = req.body.username;
-    let givenPassword = req.body.password;
-    let foundUser = db.getUserByUsername(givenUsername);
-    if (foundUser && foundUser.password === givenPassword) {
-      req.session.whoami = givenUsername;
-      res.redirect("/");
-    } else {
-      res.redirect("login");
-    }
-  })
-  
-  router.get("/login", (req, res) => {
-    let user = db.getUserByUsername(req.session.whoami); 
-    res.render("login", { user });
-  })
-  
-  router.post("/logout", (req, res) => {
-    res.clearCookie("whoami");
-    res.clearCookie("whoami.sig");
+router.post("/login", async (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let foundUser = await dbModel.getUser(email)
+  console.log(foundUser)
+  if (foundUser && foundUser.password === givenPassword) {
+    req.session.whoami = givenUsername;
     res.redirect("/");
-  })
+  } else {
+    res.redirect("login");
+  }
+})
+
+router.get("/login", (req, res) => {
+  let user = dbModel.getUser();
+  res.render("login", { user });
+})
+
+router.post("/logout", (req, res) => {
+  res.clearCookie("whoami");
+  res.clearCookie("whoami.sig");
+  res.redirect("/");
+})
 
 module.exports = router;
