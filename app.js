d11110e1c2eea0e9e6fee9c295dc747dd8ce6488
Gwanggyo Oh <goh3@my.bcit.ca>
2022-04-26 11:38:48 -0700

    database changed

--- app.js
+++ app.js
@@ -28,44 +28,38 @@ app.use("/subs", subs);
 app.use("/comments", comments);
 
 app.get("/", (req, res) => {
-  //   let getuser = dbModel.getUser(req.session.whoami)
-  //   console.log(allUsers)
-  //   res.render("index", { Allusers: results });
-  // })
-
-  let posts = db.getPosts();
-  let user = db.getUserByUsername(req.session.whoami);
-  for (const post of posts) {
-    let user = db.getUser(post.creator);
-    post.creatorName = user.uname;
-  }
 
   res.render("index", {});
 })
+// app.get("/login", (req, res) => {
 //   console.log("page hit");
-//   database.getConnection(function (err, dbConnection) {
-//     if (err) {
-//       res.render('error', { message: 'Error connecting to MySQL' });
-//       console.log("Error connecting to mysql");
-//       console.log(err);
-//     }
-//     else {
+//   let userId = req.query.id;
+//   console.log(userId)
+//   let user = dbModel.getUser(userId)
+//   if (user === null) {
+//     res.render("error", { message: "Error connecting to MySQL" });
+//     console.log("Error connecting to userModel");
+//   } else {
+//     console.log(user)
+//     res.render("login", { user });
+//   }
+// })
+app.get("/:id", (req, res) => {
+  const id = +req.params.id;
+  dbModel.getUser(id, (error, user) => {
+    if (error) {
+      console.error(error);
+      res.status(500), send({ error });
+      return;
+    }
+    if (!user) {
+      res.status(400).send({ error: `there is no user with this id ${id}` });
+      return;
+    }
+    res.send(user);
+  });
+});
 
-//       dbModel.getAllUsers((err, result) => {
-//         if (err) {
-//           res.render('error', { message: 'Error reading from MySQL' });
-//           console.log("Error reading from mysql");
-//           console.log(err);
-//         }
-//         else { //success
-//           res.render('index', { allUsers: result });
-//           //Output the results of the query to the Heroku Logs
-//           console.log(result);
-//         }
-//       });
-//       dbConnection.release();
-//     }
-//   });
-// });
+
 
 module.exports = app;
