46a4d5e1c5dd814083b1ec685b8e81438d2a9b3c
Gwanggyo Oh <goh3@my.bcit.ca>
2022-04-29 00:56:22 -0700

    clean the code and hashed password

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -1,5 +1,4 @@
 const database = require('./databaseConnection');
-const passwordPepper = "SeCretPeppa4MySal+";
 
 async function getUsers() {
     const [users] = await database.query("SELECT * FROM foodie_user");
@@ -9,12 +8,12 @@ async function getUsers() {
 async function getUser(email) {
     let query = `SELECT * FROM foodie_user WHERE email = :email`;
     let params = { email: email }
-    const users = await database.query(query, params)
-    return users
+    const user = await database.query(query, params)
+    return user[0][0]
 }
 async function getImgs() {
-    const [rows] = await database.query("SELECT * FROM posts");
-    return rows;
+    const [imgs] = await database.query("SELECT * FROM posts");
+    return imgs;
 }
 async function getImg(id) {
     const query = `SELECT * FROM posts WHERE post_id = ?`
@@ -29,54 +28,12 @@ async function addImg(description, image_url, url) {
     return result
 }
 
-// need to make async later
-function addUser(postData, callback) {
-    let sqlInsertSalt = "INSERT INTO foodie_user (email, password_salt) VALUES (:email, sha2(UUID(),512));";
-    let params = {
-        email: postData.email
-    };
-    console.log(sqlInsertSalt);
-    database.query(sqlInsertSalt, params, (err, results, fields) => {
-        if (err) {
-            console.log(err);
-            callback(err, null);
-        }
-        else {
-            let insertedID = results.insertId;
-            let updatePasswordHash = "UPDATE foodie_user SET password_hash = sha2(concat(:password,:pepper,password_salt),512) WHERE user_id = :userId;"
-            let params2 = {
-                password: postData.password,
-                pepper: passwordPepper,
-                userId: insertedID
-            }
-            console.log(updatePasswordHash);
-            database.query(updatePasswordHash, params2, (err, results, fields) => {
-                if (err) {
-                    console.log(err);
-                    callback(err, null);
-                } else {
-                    console.log(results);
-                    callback(null, results);
-                }
-            });
-        }
-    });
+async function addUser(firtst_name, last_name, email, password) {
+    const query = "INSERT INTO foodie_user (first_name, last_name, email, password) VALUES(?, ?, ?, ?)"
+    const params = [firtst_name, last_name, email, password]
+    const [result] = await database.query(query, params)
+    return result
 }
 
-function deleteUser(webUserId, callback) {
-    let sqlDeleteUser = "DELETE FROM foodie_user WHERE user_id = :userID";
-    let params = {
-        userID: webUserId
-    };
-    console.log(sqlDeleteUser);
-    database.query(sqlDeleteUser, params, (err, results, fields) => {
-        if (err) {
-            callback(err, null);
-        } else {
-            console.log(results);
-            callback(null, results);
-        }
-    });
-}
 
-module.exports = { getUsers, getUser, addUser, deleteUser, addImg, getImg, getImgs }
+module.exports = { getUsers, getUser, addUser, addImg, getImg, getImgs }
--- routes/posts.js
+++ routes/posts.js
@@ -1,93 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../fake-db");
+const database = require("../databaseConnection")
+const dbModel = require("../databaseAccessLayer")
 
-router.get("/", (req, res) => {
-    let id = req.session.whoami
-    res.render("post", { id });
+router.post("/create", async (req, res) => {
+    const connection = await database.getConnection()
+    const description = req.body.description
+    const image_url = req.body.image_url
+    const url = req.body.url
+    const email = req.session.whoami
+    const addImg = await dbModel.addImg(description, image_url, url)
+    res.redirect("/posts")
+    connection.release()
 })
-
-router.get("/create", (req, res) => {
-    let id = req.session.whoami;
-    let user = db.getUserByUsername(id);
-    res.render("newpost", { user });
+router.get("/", async (req, res) => {
+    const connection = await database.getConnection()
+    const email = req.session.whoami
+    const user = await dbModel.getUser(email)
+    const Imgs = await dbModel.getImgs()
+    res.render("post", { Imgs, user });
+    connection.release()
 })
-
-router.post("/create", (req, res) => {
-    let id = req.session.whoami;
-    let user = db.getUserByUsername(id);
-    let data = req.body;
-    let title = data.title;
-    let link = data.link;
-    let creator = user.id;
-    let description = data.description;
-    let subgroup = data.subgroup;
-    db.addPost(title, link, creator, description, subgroup);
-    res.redirect("/");
+router.get("/create", async (req, res) => {
+    const connection = await database.getConnection()
+    const email = req.session.whoami
+    const user = await dbModel.getUser(email)
+    const Imgs = await dbModel.getImgs()
+    res.render("newpost", { Imgs, user });
+    connection.release()
 })
-
-router.get("/edit/:postid", (req, res) => {
-    let postId = req.params.postid;
-    let post = db.getPost(postId);
-    let id = req.session.whoami
-    let user = db.getUserByUsername(id);
-    res.render("edit", { post, user });
-})
-
-router.post("/edit/:postid", (req, res) => {
-    let postId = req.params.postid;
-    let editedData = req.body;
-    let id = req.session.whoami;
-    let user = db.getUserByUsername(id);
-    let post = db.getPost(postId);
-    if (user.id === post.creator.id) {
-        db.editPost(postId, editedData);
-        res.redirect(`/posts/show/${postId}`);
-    } else {
-        res.redirect("/");
+router.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
     }
-})
-
-router.get("/deleteconfirm/:postid", (req, res) => {
-    let postId = req.params.postid;
-    let post = db.getPost(postId);
-    let id = req.session.whoami;
-    let user = db.getUserByUsername(id);
-    res.render("delete", { user, post });
-})
-
-router.post("/delete/:postid", (req, res) => {
-    let postId = req.params.postid;
-    let post = db.getPost(postId);
-    let postCreator = post.creator;
-    let id = req.session.whoami;
-    let user = db.getUserByUsername(id);
-    if (postCreator.id === user.id) {
-        db.deletePost(post.id);
-        res.redirect("/subs/list");
-    } else {
-        res.redirect(`/posts/show/${postId}`);
-    }
-})
-
-router.post("/comment-create/:postid", (req, res) => {
-    let postId = req.params.postid;
-    let data = db.getPost(postId);
-    let user = db.getUserByUsername(req.session.whoami);
-    let creator = user.id;
-    let description = req.body.comment;
-    db.addComment(postId, creator, description);
-    res.redirect(`/posts/show/${postId}`)
-})
-
-router.post("/vote/:postid", (req, res) => {
-    let postId = req.params.postid;
-    let vote = req.body.setvoteto;
-    let id = req.session.whoami;
-    let post = db.getPost(postId);
-    let user = db.getUserByUsername(id);
-    db.addVoteForPost(postId, user.id, Number(vote));
-    res.redirect(`/posts/show/${postId}`)
-})
-
+    console.error(err);
+    res.status(500).send({ error: "something bad happened" });
+});
 module.exports = router;
