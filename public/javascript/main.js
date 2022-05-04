eb179334d5987564456f9612698b8ff94b05fd1c
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2022-05-03 23:04:00 -0700

    Update main.js

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,17 +1,29 @@
 let horizontalUnderline = document.getElementsByClassName("horizontal-underline")[0]
-let horizontalMenus = document.querySelectorAll("nav");
-
-horizontalMenus.forEach(menu=>menu.addEventListener("mouseover",(e)=>horizontalIndicator(e)));
+console.log(horizontalUnderline)
+let horizontalMenus = document.querySelectorAll("nav a");
+console.log(horizontalMenus)
 
 function horizontalIndicator(e) {
   console.log("it's working!")
-    horizontalUnderline.style.left = e.target.offsetLeft +"px";
-    horizontalUnderline.style.width = e.target.offsetWidth +"px";
-    horizontalUnderline.style.top = e.target.offsetTop+ e.currentTarget.offsetHeight +"px";
-
+  horizontalUnderline.style.left = e.target.offsetLeft +"50px";
+  horizontalUnderline.style.width = e.target.offsetWidth +"px";
+  horizontalUnderline.style.top = e.target.offsetTop+ e.currentTarget.offsetHeight +"px";
 }
+
+horizontalMenus.forEach(menu=>menu.addEventListener("mouseover",(e)=>horizontalIndicator(e)));
 // horizontalMenus.forEach((menu) =>
 //   menu.addEventListener("click", (e) =>
 //     horizontalIndicator(e)
 //   )
 // );
+
+
+
+
+
+
+
+
+
+
+
