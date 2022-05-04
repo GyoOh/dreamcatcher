086a06fbf6d03fce1e7298fabe3ed26f229b2221
Sejin Oh <soh52@my.bcit.ca>
2022-05-04 13:33:44 -0700

    bug fixed

--- routes/posts.js
+++ routes/posts.js
@@ -20,7 +20,7 @@ router.post("/create", upload.single("image"), async (req, res) => {
     const connection = await database.getConnection()
     const user = await dbModel.getUser(req.session.whoami)
     const { filename, path } = req.file
-    const description = req.params.description;
+    const description = req.body.description
     const url = await s3.uploadFile(req.file)
     const image_url = `https://direct-upload-s3-bucket-idsp.s3.us-west-2.amazonaws.com/${url.Key}`
     await dbModel.addPost(user.user_id, description, image_url)
