b6a96f022700dfce5b9ec3a12044647b37146f1c
Gwanggyo Oh <goh3@my.bcit.ca>
2022-05-04 13:05:54 -0700

    updated database

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -5,17 +5,18 @@ console.log(horizontalMenus)
 
 function horizontalIndicator(e) {
   console.log("it's working!")
-  horizontalUnderline.style.left = e.target.offsetLeft +"50px";
-  horizontalUnderline.style.width = e.target.offsetWidth +"px";
-  horizontalUnderline.style.top = e.target.offsetTop+ e.currentTarget.offsetHeight +"px";
+  horizontalUnderline.style.left = e.target.offsetLeft + "50px";
+  horizontalUnderline.style.width = e.target.offsetWidth + "px";
+  horizontalUnderline.style.top = e.target.offsetTop + e.currentTarget.offsetHeight + "px";
 }
 
-horizontalMenus.forEach(menu=>menu.addEventListener("mouseover",(e)=>horizontalIndicator(e)));
+horizontalMenus.forEach(menu => menu.addEventListener("mouseover", (e) => horizontalIndicator(e)));
 // horizontalMenus.forEach((menu) =>
 //   menu.addEventListener("click", (e) =>
 //     horizontalIndicator(e)
 //   )
 // );
+
 
 
 
