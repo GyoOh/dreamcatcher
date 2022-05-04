d49b708292a9ccea287e0c5a1a8a3f713f94e3cb
GyoOh <90816351+GyoOh@users.noreply.github.com>
2022-05-03 19:57:51 -0700

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -18,10 +18,8 @@ const upload = multer({ storage: storage })
 
 router.post("/create", upload.single("image"), async (req, res) => {
     const connection = await database.getConnection()
-    const email = req.session.whoami
-    const user = await dbModel.getUser(email)
+    const user = await dbModel.getUser(req.session.whoami)
     const { filename, path } = req.file
-    // const description = req.body.description
     const url = await s3.uploadFile(req.file)
     const image_url = `https://direct-upload-s3-bucket-idsp.s3.us-west-2.amazonaws.com/${url.Key}`
     await dbModel.addPost(user.user_id, image_url)
@@ -30,32 +28,39 @@ router.post("/create", upload.single("image"), async (req, res) => {
 })
 
 router.get("/create", async (req, res) => {
-    const connection = await database.getConnection()
-    const email = req.session.whoami
-    const user = await dbModel.getUser(email)
+    const user = await dbModel.getUser(req.session.whoami)
+    if (!user) {
+        return res.redirect("/authentication/403");
+    }
     const posts = await dbModel.getPosts(user.user_id)
     res.render("newpost", { posts, user });
-    connection.release()
 })
 
 router.get("/", async (req, res) => {
-    const connection = await database.getConnection()
-    const email = req.session.whoami
-    const getUsers = await dbModel.getUsers()
-    const user = await dbModel.getUser(email)
+    const user = await dbModel.getUser(req.session.whoami)
+    if (!user) {
+        return res.redirect("/authentication/403");
+    }
     const posts = await dbModel.getPosts(user.user_id)
-    console.log(posts)
-    res.render("post", { posts, getUsers, user });
-    connection.release()
+    res.render("post", { posts, user });
 })
 
-
+router.post("/comment", async (req, res) => {
+    const connection = await database.getConnection()
+    const email = req.session.whoami
+    const user = await dbModel.getUser(email)
+    if (user) {
+        await dbModel.getPost(user.user_id)
+        await dbModel.addcomment(user.user_id, post.post_id, comments)
+    }
+    res.redirect("/posts")
+    connection.release()
+})
 
 router.use((err, req, res, next) => {
     if (res.headersSent) {
         return next(err);
     }
-
     console.error(err);
     res.status(500).send({ error: "something bad happened" });
 });
