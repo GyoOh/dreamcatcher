3b0d335206dfee75a91b7c97ca6c90c8216121a1
Gwanggyo Oh <goh3@my.bcit.ca>
2022-05-03 20:01:01 -0700

    changed

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -25,9 +25,9 @@ async function getPost(user_id) {
     return img
 }
 
-async function addPost(user_id, description, image_url) {
-    let query = "INSERT INTO posts (user_id, description, image_url) VALUES(?, ?, ?)"
-    const params = [user_id, description, image_url]
+async function addPost(user_id, image_url) {
+    let query = "INSERT INTO posts (user_id, image_url) VALUES(?, ?, ?)"
+    const params = [user_id, image_url]
     const [result] = await database.query(query, params)
     return result
 }
