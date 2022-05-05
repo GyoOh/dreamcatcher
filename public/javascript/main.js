1ff127c30544eef29a0ec131976f2bcb99b042ca
Gwanggyo Oh <goh3@my.bcit.ca>
2022-05-05 00:33:51 -0700

    bug fixed

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -3,16 +3,33 @@ console.log(horizontalUnderline)
 let horizontalMenus = document.querySelectorAll("nav a");
 console.log(horizontalMenus)
 
-horizontalMenus.forEach(menu=>menu.addEventListener("mouseover",(e)=>horizontalIndicator(e)));
+horizontalMenus.forEach(menu => menu.addEventListener("mouseover", (e) => horizontalIndicator(e)));
 
 function horizontalIndicator(e) {
   console.log("it's working!")
-  horizontalUnderline.style.left = e.target.offsetLeft +"px";
-  horizontalUnderline.style.width = e.target.offsetWidth +"px";
-  horizontalUnderline.style.top = 
-  e.target.offsetTop+ e.target.offsetHeight +"px";
+  horizontalUnderline.style.left = e.target.offsetLeft + "px";
+  horizontalUnderline.style.width = e.target.offsetWidth + "px";
+  horizontalUnderline.style.top =
+    e.target.offsetTop + e.target.offsetHeight + "px";
 }
+const header = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+const body = JSON.stringify({
+  comments
+});
+const request = {
+  method: "POST",
+  headers: header,
+  body: body,
+};
 
+fetch("/posts", request)
+  .then((resp) => resp.json())
+  .then((resp) => {
+    console.log(resp)
+  });
 
 
 
