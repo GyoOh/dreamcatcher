bc546b5b5e1b79bc37fd1d008db612e26c6d5623
Sejin Oh <soh52@my.bcit.ca>
2022-05-24 16:48:33 -0700

    search bar added

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -178,6 +178,10 @@ function initMap() {
         
     ];
 
+    const searchInput = document.createElement('input');
+    const searchBox = new google.maps.places.SearchBox(searchInput);
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(searchInput);
+    searchInput.placeholder = "Enter a place";
 
     for (let i = 0; i < markers.length; i++) {
         console.log('test', i)
@@ -209,7 +213,7 @@ function initMap() {
             });
 
         } else {
-            infoWindow.remove(map, marker)
+            // infoWindow.remove(map, marker)
         }
     }
 
@@ -329,13 +333,91 @@ function clearRoute() {
 
 }
 
-function handleYelpRequest() {
-    const request = {
-        method: "GET",
-    }
-    fetch(`/location`, request)
-        .then(resp =>
-            resp.json()
-        )
-}
-handleYelpRequest()
+// function handleYelpRequest() {
+//     const request = {
+//         method: "GET",
+//     }
+//     fetch(`/location`, request)
+//         .then(resp =>
+//             resp.json()
+//         )
+// }
+// handleYelpRequest()
+
+// function initAutocomplete() {
+//     const options = {
+//         zoom: 13,
+//         center: { lat: 49.282359695758885, lng: -123.1168886758965 },
+//         mapId: 'd2716697ffbc4fa6'
+//     }
+//     const map = new
+//         google.maps.Map(document.getElementById('map'), options);
+  
+//     // Create the search box and link it to the UI element.
+//     const input = document.getElementById("pac-input");
+//     const searchBox = new google.maps.places.SearchBox(input);
+  
+//     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  
+//     // Bias the SearchBox results towards current map's viewport.
+//     map.addListener("bounds_changed", () => {
+//       searchBox.setBounds(map.getBounds());
+//     });
+  
+//     let markers;
+  
+//     // Listen for the event fired when the user selects a prediction and retrieve
+//     // more details for that place.
+//     searchBox.addListener("places_changed", () => {
+//       const places = searchBox.getPlaces();
+  
+//       if (places.length == 0) {
+//         return;
+//       }
+  
+//       // Clear out the old markers.
+//       markers.forEach((marker) => {
+//         marker.setMap(null);
+//       });
+//       markers = [];
+  
+//       // For each place, get the icon, name and location.
+//       const bounds = new google.maps.LatLngBounds();
+  
+//       places.forEach((place) => {
+//         if (!place.geometry || !place.geometry.location) {
+//           console.log("Returned place contains no geometry");
+//           return;
+//         }
+  
+//         const icon = {
+//           url: place.icon,
+//           size: new google.maps.Size(71, 71),
+//           origin: new google.maps.Point(0, 0),
+//           anchor: new google.maps.Point(17, 34),
+//           scaledSize: new google.maps.Size(25, 25),
+//         };
+  
+//         // Create a marker for each place.
+//         markers.push(
+//           new google.maps.Marker({
+//             map,
+//             icon,
+//             title: place.name,
+//             position: place.geometry.location,
+//           })
+//         );
+  
+//         if (place.geometry.viewport) {
+//           // Only geocodes have viewport.
+//           bounds.union(place.geometry.viewport);
+//         } else {
+//           bounds.extend(place.geometry.location);
+//         }
+//       });
+//       map.fitBounds(bounds);
+//     });
+//   }
+// window.initAutocomplete = initAutocomplete;
+// console.log('zzz',window.initAutocomplete)
+  
