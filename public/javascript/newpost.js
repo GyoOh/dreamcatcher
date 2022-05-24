cec7d1d6514ef9e9c826277b5514ad89fa41bc37
Gwanggyo Oh <goh3@my.bcit.ca>
2022-05-23 17:56:23 -0700

    changed

--- public/javascript/newpost.js
+++ public/javascript/newpost.js
@@ -1,35 +1,13 @@
 
 
 const getRestaurant = async () => {
-    let url = new URL(`https://cryptic-headland-94862.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=chicken&latitude=49.2827&longitude=-123.1207`)
+    let url = new URL(`https://api.yelp.com/v3/businesses/search?term=chicken&latitude=49.2827&longitude=-123.1207`)
     let header = new Headers({
         "Authorization": `Bearer ROF0HVCZJhK3MOwM_BdaB_bIodzpNbWdhHMDsXZxF7bRg35xwwQRscs_ZJQdV7HKKonIdb5iyHpfY-sabDbugiUfBkDDg4tVymAhpAx7Rs8ratmrpPnMW3hqMtSJYnYx`,
     })
     let response = await fetch(url, { headers: header })
     let data = await response.json()
-    console.log(url)
+    console.log(data)
 }
 getRestaurant()
-
-
-// const header = {
-//     Accept: "application/json",
-//     "Content-Type": "application/json",
-//     "Authorization": "Bearer ROF0HVCZJhK3MOwM_BdaB_bIodzpNbWdhHMDsXZxF7bRg35xwwQRscs_ZJQdV7HKKonIdb5iyHpfY-sabDbugiUfBkDDg4tVymAhpAx7Rs8ratmrpPnMW3hqMtSJYnYx"
-// }
-// let body = JSON.stringify({
-//     post_id
-// });
-// const request = {
-//     url,
-//     method: "POST",
-//     headers: header,
-//     body: body,
-// }
-// fetch(`/posts/${post_id}/like`, request)
-//     .then(resp => resp.json())
-//     .then((data) => {
-//         console.log(data)
-//     })
-//     .catch(err => console.log(err))
 
