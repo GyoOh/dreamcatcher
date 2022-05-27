1004bf79e9beba9863e3aec138dac9c883f69fc4
Sejin Oh <soh52@my.bcit.ca>
2022-05-27 14:40:21 -0700

    change

--- public/javascript/newpost.js
+++ public/javascript/newpost.js
@@ -41,3 +41,6 @@ document.querySelector("img.plus-icon").addEventListener("click", event => {
         .catch(err => console.log(err))
 })
 
+document.querySelector(".preview").addEventListener("click", (event) => {
+    document.querySelector("#id_photo").click();
+})
